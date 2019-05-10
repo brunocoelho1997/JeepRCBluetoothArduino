2cd7dab43edecd59b49d2d1c176be8d49df5fd10
BrunoCoelho <brunocoelho1997@gmail.com>
2019-05-10 15:32:44 +0100

    Fix on imports

---
+++ JeepRcBluetooth/Utils.h
@@ -0,0 +1,8 @@
+#ifndef Utils_h
+#define Utils_h
+
+//constants
+extern const int hightGear = 1;
+extern const int lowGear = 2;
+
+#endif
--- Utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef Utils_h
-#define Utils_h
-
-//constants
-extern const int hightGear = 1;
-extern const int lowGear = 2;
-
-#endif
